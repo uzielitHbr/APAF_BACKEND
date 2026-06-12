9d6dfd80f2349c8b9ebcc2b7074ee40e56636fe4
uziel <20243ds112@utez.edu.mx>
2026-06-12 11:33:42 -0600

    v1- WebSecurity 12-JUN-2026

--- src/main/java/app/apaf/backend/core/security/WebSecurityConfig.java
+++ src/main/java/app/apaf/backend/core/security/WebSecurityConfig.java
@@ -1,18 +1,24 @@
 package app.apaf.backend.core.security;
 
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.Arrays;
+import java.util.List;
 
 
 /*
@@ -32,12 +38,51 @@ public class WebSecurityConfig {
    private final AuthenticationProvider authenticationProvider;
 
    @Bean
-   public SecurityWebFilterChain filterChain(ServerHttpSecurity http) throws Exception {
+   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
       http
-              .csrf(AbstractHttpConfigurer::disable)
-              .authorizeHttpRequest( auth -> auth
-                      .req)
+              .authorizeHttpRequests(
+                      command ->
+                              command.requestMatchers(
+                              "/api/auth/**").permitAll()
+                              .anyRequest().authenticated()
+                      )
+              .sessionManagement(
+                      session->
+                              session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+              )
+              .csrf(csrf -> csrf.disable())
+              .exceptionHandling(
+              exception ->
+                      exception
+                      .authenticationEntryPoint((request, response, authException) -> {
+                         response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+                      }) )
+              .headers(headers->
+                      headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
+              .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+              .authenticationProvider(authenticationProvider)
+              .cors(cors ->
+                      cors.configurationSource(corsConfigurationSource()));
 
+
+      return http.build();
+   }
+
+   @Bean
+   public CorsConfigurationSource corsConfigurationSource() {
+      CorsConfiguration configuration = new CorsConfiguration();
+      configuration.setAllowedOrigins(List.of(
+              "http://localhost:8080",
+              "https://unpoetically-interramal-loren.ngrok-free.dev"
+
+      ));
+      configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+      configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type","ngrok-skip-browser-warning"));
+      configuration.setAllowCredentials(true);
+
+      UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+      source.registerCorsConfiguration("/**", configuration);
+      return source;
    }
 
 
